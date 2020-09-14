d2b116f4d74ecbe354e0c8c1151ebef6d52c5b2d
Will Freyman <willfreyman@gmail.com>
2020-09-13 18:34:48 -0700

    another tweak to pop sizes

--- run_sims/run_sims.sh
+++ run_sims/run_sims.sh
@@ -1,7 +1,7 @@
 
-parallel -j 50 echo "'sim_num={1}; pop_size={2}; seed({1}); source(\"msc_sims.Rev\");' | rb" ::: {1..100} ::: 0.001 0.1 0.5 1
+parallel -j 50 echo "'sim_num={1}; pop_size={2}; seed({1}); source(\"msc_sims.Rev\");' | rb" ::: {1..100} ::: 0.001 0.1 0.5 1 2
 
-for i in 0.001 0.1 0.5 1
+for i in 0.001 0.1 0.5 1 2
 do
     cd nindv_"$i"
     time parallel --eta -j 50 echo "'sim_num={}; seed({}); source(\"../msc_inference.Rev\");' | rb" ::: {1..100}
