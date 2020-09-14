19094460751fb6e4711b819d8eea794485b612a1
Will Freyman <willfreyman@gmail.com>
2020-09-13 17:22:37 -0700

    running sim reps w/ new pop sizes

--- run_sims/run_sims.sh
+++ run_sims/run_sims.sh
@@ -1,10 +1,10 @@
 
-parallel -j 12 echo "'sim_num={1}; pop_size={2}; seed({1}); source(\"../msc_sims.Rev\");' | rb" ::: {1..100} ::: 1 2 10 50 #100
+parallel -j 50 echo "'sim_num={1}; pop_size={2}; seed({1}); source(\"msc_sims.Rev\");' | rb" ::: {1..100} ::: 0.001 0.1 0.5 1
 
-for i in 1 2 10 50 #100
+for i in 0.001 0.1 0.5 1
 do
-cd nindv_"$i"
-time parallel --eta -j 25 echo "'sim_num={}; seed({}); source(\"../../msc_inference.Rev\");' | rb" ::: {1..100}
-cd ..
+    cd nindv_"$i"
+    time parallel --eta -j 50 echo "'sim_num={}; seed({}); source(\"../msc_inference.Rev\");' | rb" ::: {1..100}
+    cd ..
 done
 
