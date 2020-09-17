64a34bf0a598aa71b4570def529bbddd3c54132e
Will Freyman <willfreyman@gmail.com>
2020-09-16 21:30:21 -0700

    many more sim tests

--- run_sims/run_sims.sh
+++ run_sims/run_sims.sh
@@ -1,10 +1,20 @@
 
-parallel -j 50 echo "'sim_num={1}; pop_size={2}; seed({1}); source(\"msc_sims.Rev\");' | rb" ::: {1..100} ::: 0.001 0.1 0.5 1 2
+parallel -j 50 echo "'sim_num={1}; pop_size={2}; source(\"msc_sims.Rev\");' | rb" ::: {1..100} ::: 0.001 0.1 0.5 1 2
+parallel -j 50 echo "'sim_num={1}; pop_size={2}; source(\"msc_sims.Rev\");' | rb" ::: {101..400} ::: 0.001
+parallel -j 50 echo "'sim_num={1}; pop_size={2}; source(\"msc_sims_seq_size.Rev\");' | rb" ::: {1..500} ::: 0.0001
 
 for i in 0.001 0.1 0.5 1 2
 do
     cd nindv_"$i"
-    time parallel --eta -j 50 echo "'sim_num={}; seed({}); source(\"../msc_inference.Rev\");' | rb" ::: {1..100}
+    time parallel --eta -j 50 echo "'sim_num={}; source(\"../msc_inference.Rev\");' | rb" ::: {1..100}
     cd ..
 done
+i=0.001
+cd nindv_"$i"
+time parallel --eta -j 50 echo "'sim_num={}; source(\"../msc_inference.Rev\");' | rb" ::: {101..400}
+cd ..
+i=0.0001
+cd nindv_"$i"
+time parallel --eta -j 50 echo "'sim_num={}; source(\"../msc_inference.Rev\");' | rb" ::: {1..500}
+cd ..
 
