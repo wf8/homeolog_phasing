a64e62df90fa0c3a949cb768d6b0fc1f55dcfa00
Will Freyman <willfreyman@gmail.com>
2020-11-30 23:43:58 -0800

    updat allelic sims inference script

--- allelic_sims/src/run_sims.sh
+++ allelic_sims/src/run_sims.sh
@@ -1,4 +1,5 @@
 
 parallel -j 50 echo "'sim_num={1}; source(\"src/sims.Rev\");' | rb" ::: {0..999} 
-time parallel --eta -j 50 echo "'sim_num={}; source(\"src/inference.Rev\");' | rb" ::: {0..999}
+time parallel --eta -j 50 echo "'sim_num={}; dummy_tip=TRUE; source(\"src/inference.Rev\");' | rb" ::: {0..999}
+time parallel --eta -j 50 echo "'sim_num={}; dummy_tip=FALSE; source(\"src/inference.Rev\");' | rb" ::: {0..999}
 
