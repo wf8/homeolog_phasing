872fb974f9662548e9e93be75a786f72fffa1280
Will Freyman <willfreyman@gmail.com>
2020-12-05 10:03:44 -0800

    BFs for allelic sims

--- allelic_sims/src/run_sims.sh
+++ allelic_sims/src/run_sims.sh
@@ -1,5 +1,7 @@
 
-parallel -j 50 echo "'sim_num={1}; source(\"src/sims.Rev\");' | rb" ::: {0..999} 
-time parallel --eta -j 50 echo "'sim_num={}; dummy_tip=TRUE; source(\"src/inference.Rev\");' | rb" ::: {0..999}
-time parallel --eta -j 50 echo "'sim_num={}; dummy_tip=FALSE; source(\"src/inference.Rev\");' | rb" ::: {0..999}
+#parallel -j 50 echo "'sim_num={1}; source(\"src/sims.Rev\");' | rb" ::: {0..999} 
+#time parallel --eta -j 50 echo "'sim_num={}; bayes_factors=FALSE; dummy_tip=TRUE; source(\"src/inference.Rev\");' | rb" ::: {0..999}
+#time parallel --eta -j 50 echo "'sim_num={}; bayes_factors=FALSE; dummy_tip=FALSE; source(\"src/inference.Rev\");' | rb" ::: {0..999}
+time parallel --eta -j 50 echo "'sim_num={}; bayes_factors=TRUE; dummy_tip=TRUE; source(\"src/inference.Rev\");' | rb" ::: {0..999}
+time parallel --eta -j 50 echo "'sim_num={}; bayes_factors=TRUE; dummy_tip=FALSE; source(\"src/inference.Rev\");' | rb" ::: {0..999}
 
