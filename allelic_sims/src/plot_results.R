9b00fb3af9e555f1aa51d96b32b840903b317c54
Will Freyman <willfreyman@gmail.com>
2022-06-04 14:52:44 -0700

    update sims BF plot

--- allelic_sims/src/plot_results.R
+++ allelic_sims/src/plot_results.R
@@ -28,10 +28,12 @@ d = data.frame(bf=bf, rep=rep, haplotypes=haplotypes)
 p = ggplot(d) + 
     geom_point(aes(x=rep, y=bf, color=haplotypes), alpha=0.7) + 
     geom_hline(yintercept=0, linetype='dashed') +
-    geom_hline(yintercept=100, linetype='dotted') +
+    geom_segment(x=50, xend=100, y=100, yend=100, linetype='dotted') +
     scale_color_manual(values=c('firebrick','grey34')) +
     ylab('Bayes factor') +
     xlab('Simulation replicate') +
+    facet_wrap(vars(haplotypes), ncol=2, scales="free") +
     theme_classic() +
-    guides(color=guide_legend(title='True number\nof haplotypes'))
-ggsave('bayes_factors_sims.pdf', p, width=5, height=4) 
+    theme(strip.background=element_blank(), strip.text=element_blank(), strip.placement = "outside") +
+    guides(color=guide_legend(title='True number\nof mul-tree tips'))
+ggsave('bayes_factors_sims2.pdf', p, width=7, height=3) 
